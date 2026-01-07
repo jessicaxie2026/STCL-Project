4faf4cb1e6b52e5fbbb5718ecc4b7ceb6045b58f
Jessica Xie <jessicalovesfeifei@gmail.com>
2026-01-07 17:55:08 -0500

    Initial backup of Arduino project

---
+++ UneditedCode/UneditedCode.ino
@@ -0,0 +1,198 @@
+int random_variable;
+int static_variable = 500;
+#define High_threshold1 500 //High threshold for starting reading signal from peaks
+#define Low_threshold1 400 //Low threshold for stopping reading signal from peaks
+#define High_threshold2 1000 //High threshold for starting reading signal from peaks
+#define Low_threshold2 700 //Low threshold for stopping reading signal from peaks
+#define pin_input1 A0 //input signal from PD1 (D2 and 935nm)
+#define pin_input2 A4 //input signal from PD2 (795nm)
+#define arraysize 2000 //size of the array for storing the data of the peaks
+#define dpin_out 6//digital output for triggering function generator
+#define dpin_in 3
+#define alpha1_ref 0.6
+#define alpha2_ref 0.50
+#define pin_output1 DAC0 //feedback signal to 935nm ECDL piezo
+#define pin_output1 DAC1 //feedback signal to 795nm DBR current
+
+unsigned long t01, t02, t1, t2, start_time, end_time, tstartsweep, time_peak, timenow, period =50;
+int signalarray[arraysize], dsignalarray[arraysize] = {};
+int i, len, counter, Range, j;
+double value1 , value2;
+float alpha1, alpha2, error1, error2, totalT, control_output1, control_output2;
+bool trigger, flag = HIGH;//Flag is for detecting the position of the peak.
+bool indicator2 = LOW; //indicator is for indicating the presence of the 795 peak
+bool lock;
+
+//----------------------------------------------
+//Define Servo Loop Variables for 795nm feedback
+int sign2 = -1; //define the sign of PID parameters. 1 is positive and -1 is negative.
+volatile float laser2_K_i = 1;
+volatile float laser2_K_p = 0.7;
+float laser2_error_signal_current;
+float laser2_error_signal_prev;
+float delta_laser2 = 0;
+float laser2_control_signal = 0;
+//----------------------------------------------
+
+void setup() {
+  Serial.begin(250000);
+  //Serial.begin(9600);
+  analogWriteResolution(12);
+  analogReadResolution(12);
+  pinMode(dpin_in, INPUT);
+  pinMode(dpin_out, OUTPUT);
+  digitalWrite(dpin_out, LOW);
+  Range = (pow(2, 12) - 1) - 200;
+}
+void loop() {
+  lock = digitalRead(dpin_in);
+  if(lock){
+  start_time = micros();
+  digitalWrite(dpin_out, HIGH);//starting point of the sweep
+  trigger = HIGH;
+  indicator2 = LOW;
+  tstartsweep = millis();
+  counter = 0;
+  do {
+    //Serial.println(counter);
+    value1 = analogRead(pin_input1);
+    value2 = analogRead(pin_input2);
+    //Serial.println(value1);
+    timenow = millis();
+    if (timenow - tstartsweep > period) { //Set the limit of time to sweep PZT
+      trigger = LOW;
+    }
+    if (value1 > High_threshold1 ) {//starting point of the peaks from PD1
+      
+      
+      time_peak = micros();
+      //Serial.println(time_peak-start_time);
+      i = 0;
+      do {
+        //Serial.println(value1);
+        signalarray[i] = value1;
+        //analogWrite(DAC1, value1);
+        value1 = analogRead(pin_input1);
+        i++;
+        //Serial.println(value1);
+      } while(value1 > Low_threshold1); 
+      end_time = micros();
+      len = i;
+      if (counter == 0) {
+        //t01 = start_time + peakfinder(len, end_time - start_time);
+        t01 = time_peak-start_time + peakfinder(len, end_time - time_peak);
+        //Serial.println("del");
+        //Serial.println(t01);
+        //trigger = LOW;
+      }
+      
+      
+      if (counter == 2) {
+        //t02 = start_time + peakfinder(len, end_time - start_time);
+        t02 = time_peak-start_time+ peakfinder(len, end_time - time_peak);
+        //Serial.println(t02);
+      }
+      counter++;  
+      }
+      if (counter == 1) {
+        if (value2 > High_threshold2 && indicator2 == LOW) { //starting point of the 795nm peak, counter>0 requires that the peak between two reference peaks.
+        indicator2 = HIGH;
+        time_peak = micros();
+        i = 0;
+        do {
+          signalarray[i] = value2;
+          //analogWrite(DAC1, value2);
+          //Serial.println(value2);
+          value2 = analogRead(pin_input2);
+          i++;
+        } while (value2 > Low_threshold2);
+        end_time = micros();
+        len = i;
+        t2 =time_peak-start_time+ peakfinder(len, end_time - time_peak);
+        //Serial.println(peakfinder(len, end_time - time_peak));
+        //Serial.println(t2);
+        //peakfinder(len, end_time - time_peak);
+        counter++;
+    }
+      }
+                  
+      
+      
+
+//
+  } while (trigger == HIGH);
+  
+//Serial.println("delt1");
+//error1 = (double)t1 - t01;
+error2 = (double)t2 - t01;
+totalT = (double)t02 - t01;
+//--------------------------------------------------feedback for 795nm DBR
+if (totalT > 0 && totalT < 160000 && error2 < totalT) {
+    alpha2 = error2 / totalT; //scaled error signal.
+    if (indicator2 == LOW || counter < 2) { //if there is no third peak, do not apply lock.
+      laser2_control_signal = 0;
+      control_output2 = (double)650 + Range / 2.0 * laser2_control_signal;
+      analogWrite(DAC0, control_output2);
+      Serial.println("No third peak");
+    }
+    else { //if there is the third peak, apply lock.
+      laser2_error_signal_current =  alpha2_ref - (double)error2 / totalT;
+      Serial.println(laser2_error_signal_current);
+      //laser_error_signal_current =  alpha_ref - 0.37;
+      delta_laser2 = sign2 * laser2_K_p * (laser2_error_signal_current - laser2_error_signal_prev) + sign2 * (laser2_K_i * laser2_error_signal_current);
+      laser2_control_signal += delta_laser2;
+      laser2_error_signal_prev = laser2_error_signal_current;
+      //Serial.println(laser2_control_signal, 4);
+      control_output2 = (double)2072.5 + Range / 2.0 * laser2_control_signal;
+      //Serial.println(control_output2, 4);
+      //      if (control_output < Range && control_output > 0) {
+      analogWrite(DAC1, control_output2);
+      //      }
+    }
+    //Serial.println(alpha2 * 10, 4);
+  }
+
+digitalWrite(dpin_out, LOW);//Change the trigger state back to LOW
+  }
+  else {
+    Serial.println("Lock disengaged");
+    control_output2 = (double)2072.5;
+    analogWrite(DAC1, control_output2);
+  }
+}
+//Serial.println("delt");
+//Serial.println(t2-t01);
+
+
+
+unsigned long peakfinder(int number, unsigned long duration) {//subfunction for finding peaks
+  //Using SG filter to determine the time of the peak.
+  unsigned long dt, peaktime;
+  flag = HIGH;
+  dt = duration / number;
+  //Serial.println("time");
+  //Serial.println(duration);
+  //Serial.println("number");
+  //Serial.println(number);
+  for (int j = 6; j < (number - 7); j++) {
+    //dsignalarray[j] = int(( 5 * signalarray[j + 5] + 4 * signalarray[j + 4] +3 * signalarray[j + 3] + 2 * signalarray[j + 2] + signalarray[j + 1] - signalarray[j - 1] - 2 * signalarray[j - 2] - 3 * signalarray[j - 3]- 4 * signalarray[j - 4] - 5 * signalarray[j - 5]));
+    dsignalarray[j] = int((6 * signalarray[j + 6] + 5 * signalarray[j + 5] + 4 * signalarray[j + 4] + 3 * signalarray[j + 3] + 2 * signalarray[j + 2] + signalarray[j + 1] - signalarray[j - 1] - 2 * signalarray[j - 2] - 3 * signalarray[j - 3] - 4 * signalarray[j - 4] - 5 * signalarray[j - 5] - 6 * signalarray[j - 6]));
+    
+    //Serial.println((dsignalarray[j]));
+    //analogWrite(DAC0, (dsignalarray[j] + 20000) * 0.08);
+    //analogWrite(DAC0, (dsignalarray[j] + 1000) * 1.8);
+    if (dsignalarray[j] <= 0 && flag) {
+      //Serial.println("peaktime");
+      
+      flag = LOW;
+      //peaktime = (j + dsignalarray[j] / (dsignalarray[j - 1] - dsignalarray[j])) * dt;//uses the method of linear interpolation near the zero crossing to calculate the peaktime
+      peaktime = (j+ dsignalarray[j] / (dsignalarray[j - 1] - dsignalarray[j])) * dt;//uses the method of linear interpolation near the zero crossing to calculate the peaktime
+      return peaktime;
+      //Serial.println(j);
+    }
+  }
+  //Serial.println(peaktime);
+  //Serial.println(dsignalarray[j]);
+  //Serial.println(dsignalarray[j-1]);
+  //return peaktime;
+}
