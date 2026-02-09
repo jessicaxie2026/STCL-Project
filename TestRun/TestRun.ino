3d2e42531e5b25ba7218520ded3d2d59e1053c3e
Wei Xie <wxie@cern.ch>
2026-02-09 17:29:31 -0500

    testrun

--- TestRun/TestRun.ino
+++ TestRun/TestRun.ino
@@ -9,10 +9,15 @@ const int sampleInterval = 1000; // Microseconds between samples
 void setup() {
   Serial.begin(115200);
   analogReadResolution(12); // Due suppo rts 12-bit ADC
+  ADC->ADC_MR |= 0x80;  // Use FREERUN mode for faster consecutive reads
+  
+  // Optional: Set a faster tracking time for high-frequency peaks
+  ADC->ADC_MR &= ~(0xF << 24); // Clear tracking time
+  ADC->ADC_MR |= (0x1 << 24);  // Set tracking time to minimum
 }
 
 void loop() {
   delayMicroseconds(10000);  
   int currentValue = analogRead(analogPin); 
-  Serial.println(currentValue);
+  
 }
