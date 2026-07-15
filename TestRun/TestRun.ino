353935ef4041898a9bbad7b01cd8aa1c37d6293a
Jessica Xie <jessicalovesfeifei@gmail.com>
2026-07-15 12:23:51 -0400

    switchCheck and thresholds

--- TestRun/TestRun.ino
+++ TestRun/TestRun.ino
@@ -3,9 +3,6 @@ const int analogPin = A8;       // Connect PCB output to A0
 const int sampleInterval = 1000; // Microseconds between samples
 
 
-
-
-
 void setup() {
   Serial.begin(115200);
   analogReadResolution(12); // Due suppo rts 12-bit ADC
@@ -14,5 +11,4 @@ void setup() {
 void loop() { 
   int currentValue = analogRead(analogPin); 
   Serial.println(currentValue);
-  
 }
